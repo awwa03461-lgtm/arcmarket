3a0e315936833c4e96b33af26749e2697d11e444
awwa03461-lgtm <awwa03461@gmail.com>
2026-06-08 19:29:31 +0330

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,5 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  webpack: (config) => {
+    // کانکتورهای آزمایشی wagmi که استفاده نمی‌کنیم و build را می‌شکنند
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      "porto/internal": false,
+      "porto": false,
+    };
+    config.externals.push("pino-pretty", "lokijs", "encoding");
+    return config;
+  },
 };
+
 module.exports = nextConfig;
